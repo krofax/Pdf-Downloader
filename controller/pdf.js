86ead82d927fe50af2a505fa41395b124ddc41fb
Blessing Krofegha <bkrofegha@gmail.com>
2019-12-31 01:58:15 +0100

    made pdf downloadable

--- controller/pdf.js
+++ controller/pdf.js
@@ -1,29 +1,29 @@
-//     const content = req.body.content
-//     doc.y = 300
-//     doc.getMaxListeners(content, 50, 50)
-//     doc.pipe(res)
-//     doc.end()
-// } 
 const fs = require('fs');
 const path = require('path')
 const PDFDocument = require('pdfkit');
 
 
 exports.pdf = (req, res) => {
-    const filename = 'test' + '.pdf';
-    const pdfPath = path.join('data', 'pdf', filename)
+
+    const filename = 'test1234'
+    const pdfPath = path.join('data', 'pdf', filename + '.pdf')
     
     const pdfDoc = new PDFDocument()
-    pdfDoc.pipe(fs.createWriteStream(pdfPath));
-    pdfDoc.pipe(res);
-
-    pdfDoc.text('Hello world!')
-    console.log('response', res)
-    res.send(res).
-
-    pdfDoc.end();
 
     res.setHeader('Content-Disposition', 'attachment; filename="' + filename + '" ')
     res.setHeader('Content-Type', 'application/pdf')
+
+    pdfDoc.pipe(fs.createWriteStream(pdfPath));
+    
+
+    const content = req.body.content
+    pdfDoc.text(content)
+
+    
+    pdfDoc.pipe(res);
+
+    pdfDoc.end();
+
+   
 
 }
